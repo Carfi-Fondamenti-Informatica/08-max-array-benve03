7764a2aaeb23ae3d65e39f43e26006c232e2fe6d
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-12 18:30:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+using namespace std;
+float array(float array[], int n){
+      float b;
+      for ( int j=0;j<n;j++){
+          if(j==0){
+              b=array[j];
+          } else if (array[j]>b){
+              b=array[j];
+          }
+      }
+      return b;
+      }
