8702ab30e1b35c55441318f860a377e55b7ea7fd
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-12 18:30:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    int n=0;
+    cout<<"inserisci la dimensione dell'array"<<endl;
+    cin>>n;
+    float array[n];
+    float x;
+    for (int i=0;i<n;i++){
+        cout<<"inserisci x.........x"<<endl;
+        cin>>x;
+        array[i]=x;
+    }
+    return 0;
 }
