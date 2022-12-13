c8e3b7596f1eb2bae0a97c7142ce9db04f23aacb
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-13 09:17:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,9 +4,7 @@ using namespace std;
 float maxarray(float num[], int a){
       float b=1;
       for ( int j=0;j<n;j++){
-          if(j==0){
-              b=num[j];
-          } else if (num[j]>b){
+          if(num[j]>b){
               b=num[j];
           }
       }
