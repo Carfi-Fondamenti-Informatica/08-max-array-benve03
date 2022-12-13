d53c2949a932c15eb2c9bd00cf24cb610fca9227
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-13 10:06:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
 int d = 0 ;
 cin >> d ;
-float num [d] ;
+float num[d] ;
 for (int i=0 ; i<d ; i++) {
-    cin >> num [i] ;
+    cin >> num[i] ;
 }
 cout << maxArray( num  , d) << endl;
     return 0;
